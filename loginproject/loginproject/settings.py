d36d1e60bd153acfaa391cdfbdb9cf73178f2cf5
prakashb-ai <prakashbodicse@gmail.com>
2024-09-01 01:23:36 +0530

    add

--- loginproject/loginproject/settings.py
+++ loginproject/loginproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0@8&fwtt_&hqhgy#k$da#%ms52de&uylc85*4sryrcd(cpeo%w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
